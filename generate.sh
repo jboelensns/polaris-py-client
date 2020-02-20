997e57c9bddc631ffca419a339244f621c0bd2f5
Jared Boelens <jboelens@netskope.com>
2020-02-20 20:29:23 +0000

    updated generate.sh

--- generate.sh
+++ generate.sh
@@ -2,17 +2,27 @@
 set -e
 
 #
+# generate.sh
+# 
+# uses a docker container to generate a python client for the polaris api
 #
-#wget --no-check-certificate http://localhost:8989/apispec_1.json
+# usage: sudo ./generate.sh [url_to_spec_file]
 #
-#
+# e.g 
+#  - use the prod spec
+#  generate.sh https://api.polaris.nskope.net/apispec_1.json
+#  - use a local dev spec
+#  generate.sh http://localhost:8989/apispec_1.json
+#  - use the spec file committed to the repo (apispec_1.json) 
+#  generate.sh 
 #
 
 if [ -z "$1" ]
 then
 	FILENAME="apispec_1.json"
 else
-    URL="https://api.polaris.nskope.net/apispec_1.json"
+    URL=$1
+    # example URL="https://api.polaris.nskope.net/apispec_1.json"
 	FILENAME="$(basename -- $URL)"
 	rm $FILENAME
 	wget --no-check-certificate $URL
