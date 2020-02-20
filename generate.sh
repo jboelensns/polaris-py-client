94b556e62d22684bd1cd73202169971661d27d0f
Jared Boelens <jboelens@netskope.com>
2020-02-20 20:13:19 +0000

    added includes

--- generate.sh
+++ generate.sh
@@ -21,8 +21,10 @@ fi
 echo "Generating file from $URL ($FILENAME)"
 
 VERSION=$(/usr/bin/jq '.info.version' apispec_1.json | perl -pe 's/(\d*.\d*.\d*)-(\d*)-(.*)/$1+$2.$3/' )
+PKG_NAME="polarisgenclient"
+PROJ_NAME="polaris-gen-client"
 
-echo "{\"packageVersion\": $VERSION, \"projectName\": \"polaris-gen-client\",\"packageName\":\"polarisgenclient\"}" > config.json
+echo "{\"packageVersion\": $VERSION, \"projectName\": \"$PROJ_NAME\",\"packageName\":\"$PKG_NAME\"}" > config.json
 
 cat config.json
 docker run --rm -v ${PWD}:/local swaggerapi/swagger-codegen-cli generate -i /local/apispec_1.json -l python -o /local/ -c /local/config.json
---
+++ include/config_helper.py
@@ -0,0 +1,36 @@
+from polarisapi.configuration import Configuration
+from polarisapi.jwt import jwt_header
+
+
+class ConfigHelper():
+
+    def __init__(self, access_id, secret_token,
+                 jwt_func=jwt_header,
+                 config=None,
+                 header="X-Polaris-Signed",
+                 host="https://api.polaris.nskope.net/api/v0.1"):
+
+        self.access_id = access_id
+        self.secret_token = secret_token
+        self.header = header
+        self.host = host
+        self.jwt_func = jwt_func
+
+        if config is None:
+            self.config = self.build_config()
+        else:
+            self.config = config
+
+    def build_config(self):
+        config = Configuration()
+        config.host = self.host
+        config.api_key[self.header] = self.get_jwt_str()
+        config.refresh_api_key_hook = self.on_refresh_api_key_hook
+        config.verify_ssl = False
+        return config
+
+    def get_jwt_str(self):
+        return self.jwt_func(access_id=self.access_id, secret_token=self.secret_token)
+
+    def on_refresh_api_key_hook(self, config):
+        config.api_key[self.header] = self.get_jwt_str()
