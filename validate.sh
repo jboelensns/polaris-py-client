a9769d5a8f12e7acb7d4ea42eec5e9eb15739945
Jared Boelens <jboelens@netskope.com>
2020-02-20 20:30:45 +0000

    updated validate.sh

--- validate.sh
+++ validate.sh
@@ -2,8 +2,34 @@
 
 set -e
 
-#rm -rf apispec_1.json
-#wget --no-check-certificate http://localhost:8989/apispec_1.json
+#
+# validate.sh
+# 
+# uses a docker container to validate the polaris api spec file
+#
+# usage: sudo ./validate.sh [url_to_spec_file]
+#
+# e.g 
+#  - use the prod spec
+#  generate.sh https://api.polaris.nskope.net/apispec_1.json
+#  - use a local dev spec
+#  generate.sh http://localhost:8989/apispec_1.json
+#  - use the spec file committed to the repo (apispec_1.json) 
+#  generate.sh 
+#
 
-docker run --rm -v ${PWD}:/local swaggerapi/swagger-codegen-cli validate -i /local/apispec_1.json
+if [ -z "$1" ]
+then
+	FILENAME="apispec_1.json"
+else
+    URL=$1
+    # example URL="https://api.polaris.nskope.net/apispec_1.json"
+	FILENAME="$(basename -- $URL)"
+	rm $FILENAME
+	wget --no-check-certificate $URL
+fi
+
+echo "Validating file from $URL ($FILENAME)"
+
+docker run --rm -v ${PWD}:/local swaggerapi/swagger-codegen-cli validate -i /local/$FILENAME
 
