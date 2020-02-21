b81452915ebbbaa508d0dbafae593ff318ddeb13
Jared Boelens <jboelens@netskope.com>
2020-02-21 20:35:17 +0000

    fixed up sample client

--- sample_client.py
+++ sample_client.py
@@ -2,9 +2,10 @@
 import os
 import yaml
 from polarisgenclient.api_client import ApiClient
-from polarisgenclient.api import IpamPrefixApi
+from polarisgenclient.api import ArpApi
 
 from polarisgenclient.config_helper import ConfigHelper
+
 
 def main():
 
@@ -14,12 +15,8 @@ def main():
     host = "http://localhost:8989"
     helper = ConfigHelper(cfg["access_id"], cfg["secret_token"], host=host)
 
-    ipam_client = IpamPrefixApi(ApiClient(helper.config))
-    results = ipam_client.api_v01_ipam_prefix_id_get(232)
-    print(results)
-
-    ipfilter_client = IpFilterApi(ApiClient(helper.config))
-    print(ipfilter_client.api_v01_ipfilter_get("", 1))
+    client = ArpApi(ApiClient(helper.config))
+    print(client.arp_get())
 
 
 if __name__ == '__main__':
